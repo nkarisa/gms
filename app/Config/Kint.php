3a7db4a8877d75804f7753035d66b07259552c32
londuso <londuso@ke.ci.org>
2025-04-04 15:37:53 +0300

    Fixed all defects for voucher and cash journal.

--- app/Config/Kint.php
+++ app/Config/Kint.php
@@ -40,7 +40,7 @@ class Kint
     |--------------------------------------------------------------------------
     */
     public string $richTheme = 'aante-light.css';
-    public bool $richFolder  = false;
+    public bool $richFolder  = false;//
     public int $richSort     = AbstractRenderer::SORT_FULL;
 
     /**
