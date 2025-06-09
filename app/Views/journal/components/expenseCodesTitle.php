638b2210049eb177e9e708ed374db70645781ab8
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-06-09 10:10:13 +0300

    WIP

---
+++ app/Views/journal/components/expenseCodesTitle.php
@@ -0,0 +1,3 @@
+<?php foreach ($accounts['expense'] as $expense_account_code) { ?>
+    <th><?= $expense_account_code; ?></th>
+<?php } ?>
---
+++ app/Views/journal/components/incomeCodesTitle.php
@@ -0,0 +1,3 @@
+<?php foreach ($accounts['income'] as $income_account_code) { ?>
+    <th><?= $income_account_code; ?></th>
+<?php } ?>
