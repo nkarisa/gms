a0eb864efd7da589ceae16a28c5e8b018e4aa78f
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-06-25 11:10:56 +0300

    Use Safina App image

--- app/Views/journal/components/bankRefPrompt.php
+++ app/Views/journal/components/bankRefPrompt.php
@@ -1,21 +1,41 @@
 <script>
-    function getUserInput(message) {
-        jQuery('#myPromptModal').modal('show', {
-            backdrop: 'true'
-        });
+    // function getUserInput(message) {
+    //     jQuery('#myPromptModal').modal('show', {
+    //         backdrop: 'true'
+    //     });
 
-        // SHOW AJAX RESPONSE ON REQUEST SUCCESS
-        //  $.ajax({
-        //      url: url,
-        //      success: function(response) {
-        //          jQuery('#modal_ajax .modal-body').html(response);
-        //      }
-        //  });
-    }
+    //     // SHOW AJAX RESPONSE ON REQUEST SUCCESS
+    //      $.ajax({
+    //          url: url,
+    //          success: function(response) {
+    //              jQuery('#modal_ajax .modal-body').html(response);
+    //          }
+    //      });
+    // }
 </script>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <p id="promptMessage">Enter your text here:</p>
+            <input type="text" class="form-control" id="promptInput">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-<div class="modal fade" id="myPromptModal" tabindex="-1" aria-labelledby="myPromptModalLabel" aria-hidden="true">
+<!-- <div class="modal fade" id="myPromptModal" tabindex="-1" aria-labelledby="myPromptModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
@@ -32,4 +52,4 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
--- app/Views/journal/components/journalAccrualClearAction.php
+++ app/Views/journal/components/journalAccrualClearAction.php
@@ -1,5 +1,9 @@
 <?php if($hasVoucherCreatePermission) {?>
-    <div data-voucher_id = "<?=$voucherId;?>" class = "btn btn-success clear_accrual">
+    <div 
+        data-voucher_id = "<?=$voucherId;?>" 
+        class = "btn btn-success clear_accrual"
+        data-toggle="modal" data-target="#exampleModal"
+    >
         <?=get_phrase('clear_accrual');?>
     </div>
 <?php } else {?>
@@ -9,64 +13,56 @@
 <?php }?>
 
 <script>
-    //   document.getElementById('showPromptButton').addEventListener('click', async () => {
-    //     const result = await bootstrapPrompt('What is your name?', 'Guest');
-    //     if (result !== null) {
-    //         alert('You entered: ' + result);
-    //     } else {
-    //         alert('Prompt was cancelled.');
+
+    // $(".clear_accrual").on('click', function(ev){
+    //     const voucherId = $(this).data('voucher_id')
+    //     const url = "<?=base_url();?>ajax/journal/clearAccrualTransaction";
+    //     let data = {
+    //         voucherId,
+    //         bankRef: ''
     //     }
-    //     });
 
-    $(".clear_accrual").on('click', function(ev){
-        const voucherId = $(this).data('voucher_id')
-        const url = "<?=base_url();?>ajax/journal/clearAccrualTransaction";
-        let data = {
-            voucherId,
-            bankRef: ''
-        }
+    //     if(voucherId == '<?=$voucherId;?>'){
 
-        if(voucherId == '<?=$voucherId;?>'){
+    //         let cnf = confirm('<?=get_phrase('verify_user_action','Are you sure you want to peform this action?');?>')
 
-            let cnf = confirm('<?=get_phrase('verify_user_action','Are you sure you want to peform this action?');?>')
+    //         if(!cnf){
+    //             alert('<?=get_phrase('action_aborted');?>')
+    //             return false;
+    //         }
 
-            if(!cnf){
-                alert('<?=get_phrase('action_aborted');?>')
-                return false;
-            }
-
-            $.post(url, data, function(response){
-                if(response.requireBankRef){
-                    let bankRef = prompt('<?=get_phrase('bank_reference_required');?>') 
-                    // getUserInput('<?=get_phrase('bank_reference_required');?>') // A more advanced interface is required here
+    //         $.post(url, data, function(response){
+    //             if(response.requireBankRef){
+    //                 let bankRef = prompt('<?=get_phrase('bank_reference_required');?>') 
+    //                 // getUserInput('<?=get_phrase('bank_reference_required');?>') // A more advanced interface is required here
                     
-                    data = {
-                        voucherId,
-                        bankRef: bankRef
-                    }
-                    $.post(url, data, function(response){
-                        if(response.message == ""){
-                            alert('Please provide all transaction requirements')
-                            return false;
-                        }
-                        if(response.success){
-                            alert(response.message)
-                            window.location.replace('<?=base_url('voucher/list');?>');
-                        }else{
-                            alert(response.message)
-                        }
-                    })
+    //                 data = {
+    //                     voucherId,
+    //                     bankRef: bankRef
+    //                 }
+    //                 $.post(url, data, function(response){
+    //                     if(response.message == ""){
+    //                         alert('Please provide all transaction requirements')
+    //                         return false;
+    //                     }
+    //                     if(response.success){
+    //                         alert(response.message)
+    //                         window.location.replace('<?=base_url('voucher/list');?>');
+    //                     }else{
+    //                         alert(response.message)
+    //                     }
+    //                 })
 
-                }else{
-                    if(response.success){
-                        alert(response.message)
-                        window.location.replace('<?=base_url('voucher/list');?>');
-                    }else{
-                        alert(response.message)
-                    }
-                }
-            })
-        }
-        ev.preventDefault()
-    })
+    //             }else{
+    //                 if(response.success){
+    //                     alert(response.message)
+    //                     window.location.replace('<?=base_url('voucher/list');?>');
+    //                 }else{
+    //                     alert(response.message)
+    //                 }
+    //             }
+    //         })
+    //     }
+    //     ev.preventDefault()
+    // })
 </script>
