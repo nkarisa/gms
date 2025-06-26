878d7e6a383d7e6930e558c4544926dd00cb2f8a
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-06-26 11:13:24 +0300

    Removed page redirection

--- app/Views/journal/components/journalAccrualClearAction.php
+++ app/Views/journal/components/journalAccrualClearAction.php
@@ -37,7 +37,7 @@ if ($hasVoucherCreatePermission) {
         
         if(voucherId == '<?= $voucherId; ?>' && (showBankAccounts || showBankReferences)){
             getUserInput('<?= get_phrase('verify_user_action', 'Are you sure you want to peform this action?'); ?>', data);
-        }else{
+        }else if(voucherId == '<?= $voucherId; ?>' && showBankAccounts == "" && showBankReferences == ""){
             $.post(url, data, function(response){
                 if(response.message == ""){
                     alert('Please provide all transaction requirements')
@@ -45,13 +45,13 @@ if ($hasVoucherCreatePermission) {
                 }
                     if(response.success){
                         alert(response.message)
-                        // window.location.replace('<?= base_url('voucher/list'); ?>');
                     }else{
                         alert(response.message)
                     }
             })
         }
     })
+
     // $(".clear_accrual").on('click', function(ev){
     //     const voucherId = $(this).data('voucher_id')
     //     const url = "<?= base_url(); ?>ajax/journal/clearAccrualTransaction";
