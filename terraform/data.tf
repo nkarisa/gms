b5ff74c6e13faf38fee8c8632e8abd8a66388f8a
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-07-02 13:25:53 +0300

    Clearing accruals with options

--- terraform/data.tf
+++ terraform/data.tf
@@ -96,7 +96,7 @@ locals {
       essential   = true
       portMappings = [
         {
-          containerPort = 80
+          containerPort = 8080
           hostPort      = 80
         }
       ]
