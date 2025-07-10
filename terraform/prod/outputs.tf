88a42b552a21fe342157b7e26144f7e35166d5bc
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-07-11 00:09:17 +0300

    Reorganized terraform

--- terraform/prod/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Output the ARN of the created ECS Task Definition
-output "task_definition_arn" {
-  description = "The Amazon Resource Name (ARN) of the Nginx ECS task definition."
-  value       = aws_ecs_task_definition.task_definition.arn
-}
