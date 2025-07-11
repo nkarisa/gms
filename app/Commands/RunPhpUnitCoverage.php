35c2c7354fa5c147248711c6353e93a96a69517f
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-07-11 11:23:34 +0300

    Remove critical vulnerability

--- app/Commands/RunPhpUnitCoverage.php
+++ app/Commands/RunPhpUnitCoverage.php
@@ -18,44 +18,44 @@ class RunPhpUnitCoverage extends BaseCommand
         // Run all App\Database\Migration to the test database
         CLI::write('Running Setup Migrations .....', 'yellow');
 
-        // $migrationOutput = shell_exec("php " . APPPATH . "../spark migrate -n App\Database\Migrations -g tests");
+        $migrationOutput = shell_exec("php " . APPPATH . "../spark migrate -n App\Database\Migrations -g tests");
 
-        // if ($migrationOutput) {
-        //     CLI::write($migrationOutput, 'yellow');
-        //     CLI::write('Migration successful', 'green');
-        // } else {
-        //     CLI::write('Failed to run migrations.', 'red');
-        // }
+        if ($migrationOutput) {
+            CLI::write($migrationOutput, 'yellow');
+            CLI::write('Migration successful', 'green');
+        } else {
+            CLI::write('Failed to run migrations.', 'red');
+        }
 
-        // // Instantiate the settings table schema
+        // Instantiate the settings table schema
 
-        // $db = \Config\Database::connect();
-        // $testingDatabase = env("database.tests.database");
-        // $db->query("CREATE TABLE IF NOT EXISTS `$testingDatabase`.`settings` (
-        //     `id` int NOT NULL AUTO_INCREMENT,
-        //     `class` varchar(255) NOT NULL,
-        //     `key` varchar(255) NOT NULL,
-        //     `value` text,
-        //     `type` varchar(31) NOT NULL DEFAULT 'string',
-        //     `context` varchar(255) DEFAULT NULL,
-        //     `created_at` datetime NOT NULL,
-        //     `updated_at` datetime NOT NULL,
-        //     PRIMARY KEY (`id`)
-        //     );");
+        $db = \Config\Database::connect();
+        $testingDatabase = env("database.tests.database");
+        $db->query("CREATE TABLE IF NOT EXISTS `$testingDatabase`.`settings` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `class` varchar(255) NOT NULL,
+            `key` varchar(255) NOT NULL,
+            `value` text,
+            `type` varchar(31) NOT NULL DEFAULT 'string',
+            `context` varchar(255) DEFAULT NULL,
+            `created_at` datetime NOT NULL,
+            `updated_at` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+            );");
 
-        // // Begin running PHPUnit tests
+        // Begin running PHPUnit tests
 
-        // CLI::write('Running PHPUnit tests with coverage report...', 'yellow');
+        CLI::write('Running PHPUnit tests with coverage report...', 'yellow');
 
-        // $command = "cd " . APPPATH . "../ && export XDEBUG_MODE=coverage  && composer test";
-        // // Execute the command
-        // $output = shell_exec($command);
+        $command = "cd " . APPPATH . "../ && export XDEBUG_MODE=coverage  && composer test";
+        // Execute the command
+        $output = shell_exec($command);
 
-        // if ($output) {
-        //     CLI::write($output, 'yellow');
-        //     CLI::write('Coverage report generated at ./build/logs/coverage.html', 'green');
-        // } else {
-        //     CLI::write('Failed to execute PHPUnit command.', 'red');
-        // }
+        if ($output) {
+            CLI::write($output, 'yellow');
+            CLI::write('Coverage report generated at ./build/logs/coverage.html', 'green');
+        } else {
+            CLI::write('Failed to execute PHPUnit command.', 'red');
+        }
     }
 }
