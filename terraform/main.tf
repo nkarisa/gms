54b824bfb528c3c619b64f81cd5ef7dfaedd04fd
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-07-17 22:18:10 +0300

    Added a aws cloud watch log group

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,6 +97,6 @@ resource "aws_ecs_service" "new_ecs_service" {
     data.aws_ecs_service.ecs_service,
     data.aws_lb_listener.safina_listener_https_443,
     data.aws_lb_target_group.safina_ecs_tg,
-    aws_cloudwatch_log_group.safina_ecs_log_group 
+    # data.aws_cloudwatch_log_group.safina_ecs_log_group 
   ]
 }
