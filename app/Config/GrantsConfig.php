b8fa1362c21cee3dcfbac45840dee97c90fed0d4
Nicodemus Karisa <nkarisa@ke.ci.org>
2026-03-11 08:29:07 +0300

    Update app

--- app/Config/GrantsConfig.php
+++ app/Config/GrantsConfig.php
@@ -30,7 +30,7 @@ class GrantsConfig extends BaseConfig
     public $preventUsingGlobalPermissionsByNonAdmins = true; // or false, depending on your default value
     public $methodToAttachPermissionToRole = 'both'; // direct, role_group, both
     public $defaultLaunchPage = 'Dashboard'; 
-    public $systemName = "Safina Grants Managememt System";
+    public $systemName = "Grants Managememt System";
     public $modules = ['system','core','grants'];
     public $maxPriorityMenuItems = 10;
     public $tableThatDontRequireHistoryFields = ['status', 'approve_item', 'approval_flow', 'ci_sessions'];
