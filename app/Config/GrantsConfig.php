80a968e15ab436c10d25441ea34d3fc3aec58c0c
Nicodemus Karisa <nkarisa@ke.ci.org>
2026-03-11 13:32:24 +0300

    Changed system title config

--- app/Config/GrantsConfig.php
+++ app/Config/GrantsConfig.php
@@ -30,7 +30,7 @@ class GrantsConfig extends BaseConfig
     public $preventUsingGlobalPermissionsByNonAdmins = true; // or false, depending on your default value
     public $methodToAttachPermissionToRole = 'both'; // direct, role_group, both
     public $defaultLaunchPage = 'Dashboard'; 
-    public $systemName = "[Grants Management System]";
+    public $systemName = "Grants Management System";
     public $modules = ['system','core','grants'];
     public $maxPriorityMenuItems = 10;
     public $tableThatDontRequireHistoryFields = ['status', 'approve_item', 'approval_flow', 'ci_sessions'];
